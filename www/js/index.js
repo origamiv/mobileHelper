405cadfdf9ab652d37e76a1c394d0a043b78e384
origamiv2 <origamiv@gmail.com>
2017-09-06 11:29:14 +0300

    last version

--- www/js/index.js
+++ www/js/index.js
@@ -1,30 +1,74 @@
-// example.js file
-// Wait for device API libraries to load
-//
-function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, false);
-}
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ 
+ function onLoad() 
+ {
+      alert('sdfds');
+ } 
+ 
+ function init(){ 
+   
+        }
+ 
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('app', this.onDeviceReady.bind(this), false);
+        var ht=document.getElementById("htmlid");  
+        ch=ht.clientHeight;  
+        cw=ht.clientWidth; 
+        //alert(hh);       
+        document.getElementById("page1").style.height=ch+'px';                       
+        
+        /*
+        document.getElementById("map").style.width=cw+'px';        
+        var maph=ch-32-100;
+        document.getElementById("map").style.height=maph+'px';   
+        ph=document.getElementById("content").clientHeight-maph;
+        document.getElementById("predp").style.width=cw+'px';
+        */
+        //document.getElementById("predp").style.height=;
+        //alert(ph);
+    //}
+    },
 
-// device APIs are available
-//
-function onDeviceReady() {
-    document.addEventListener("pause", onPause, false);
-    document.addEventListener("resume", onResume, false);
-    document.addEventListener("menubutton", onMenuKeyDown, false);
-    alert('111');
-    // Add similar listeners for other events
-}
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('app');
+        //alert('22'); 
+        
+    },      
 
-function onPause() {
-    // Handle the pause event
-}
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-function onResume() {
-    // Handle the resume event
-}
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-function onMenuKeyDown() {
-    // Handle the menubutton event
-}
+        console.log('Received Event: ' + id);
+    }
+};
 
-// Add similar event handlers for other events
+app.initialize();
